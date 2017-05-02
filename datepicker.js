32270f2fdd075a5fe76abc1e94875f023334ae49
Mridul Ahuja <mridul.ahuja@gmail.com>
2017-05-02 23:26:48 +0530

    Update js, css
    Add support for time
    Update UI
    Change name

--- datepicker.js
+++ datepicker.js
@@ -1,7 +1,5 @@
 /*!
- * Broomberg date picker v1.0.1
- *
- * Copyright Broomberg Cleaning Services Pvt Ltd
+ *  Elite Date Picker v2.0.0
  *
  * Author: Mridul Ahuja
  */
@@ -21,7 +19,10 @@
 
 				params = $.extend(defaults, params);
 
-				generate_datepicker(dateInput, selector, params);
+				dateInput.each(function(){
+					generate_datepicker($(this), selector, params);
+				});
+				
 			}
 			else { /* destroy */
 				destroy_datepicker(dateInput);
@@ -36,18 +37,22 @@
 			'October', 'November', 'December'];
 		var dayList = ['S', 'M', 'T', 'W', 'Th', 'F', 'Sa'];
 		var dayMap = {'Sun': 1, 'Mon': 2, 'Tue': 3, 'Wed': 4, 'Thu': 5, 'Fri': 6, 'Sat': 7};
-		var datepicker = $(`<div class="broomberg-d-p date-picker" tabindex="0" style="display: none;">
+		var datepicker = $(`<div class="mr-elite-d-p date-picker" tabindex="0" style="display: none;">
 			<span class="up-tip"></span></div>`);
 		var datepickerHeader = $(`<div class="header"></div>`);
 		var mainContent = $(`<div class="main-content"><div class="m-c-items date-selector"><div class="days"></div>
-			<div class="dates"></div><div class="ok-button"><span>OK</span></div></div>
-			<div class="m-c-items month-selector"></div><div class="m-c-items year-selector"></div></div>`);
+			<div class="dates"></div><div class="footer-buttons"><span class="set-time-button">Set Time</span>
+			<span class="ok-button">OK</span></div></div><div class="m-c-items month-selector"></div>
+			<div class="m-c-items year-selector"></div><div class="m-c-items time-selector"></div></div>`);
 		var monthSelectorButton;
 		var selectMonthContainer;
 		var selectMonth;
 		var yearSelectorButton;
 		var selectYearContainer;
 		var selectYear;
+		var timeSelectorContainer;
+		var hourSelector;
+		var minuteSelector;
 		var monthFirstDate = new Date();
 		var inputboxDate = dateInput.val() ? dateInput.val() : new Date().toJSON().slice(0,10);
 		var inputboxDateFormatted;
@@ -58,6 +63,8 @@
 		var currentMonthSelected;
 		var currentDateSelected;
 		var scrollToPos;
+		var setTimeButton;
+		var setTime;
 		var okButton;
 
 
@@ -71,6 +78,7 @@
 
 
 		var format_date_Y_m_d = (date, format) => {
+			format = format.substr(0, 5);
 			var formattedDate;
 			date[0] = parseInt(date[0]);
 			date[1] = parseInt(date[1]);
@@ -133,10 +141,15 @@
 
 
 		var get_new_date = () => {
-			var newDate = params.format;
-			newDate = newDate.replace('d', pad_date(currentDateSelected, 2));
-			newDate = newDate.replace('m', pad_date(currentMonthSelected, 2));
-			newDate = newDate.replace('Y', currentYearSelected);
+			var newDate = params.format.substr(0, 5);
+			
+				newDate = newDate.replace('d', pad_date(currentDateSelected, 2));
+				newDate = newDate.replace('m', pad_date(currentMonthSelected, 2));
+				newDate = newDate.replace('Y', currentYearSelected);
+
+			if(params.time) {
+				newDate += ' ' + setTime;
+			}
 			return newDate;
 		};
 
@@ -232,6 +245,93 @@
 		};
 
 
+		var read_time = () => {
+			mainContent.find(`.time-selector .select-hours .hour`).removeClass('selected');
+			mainContent.find(`.time-selector .select-minutes .minute`).removeClass('selected');
+
+			var timeString = dateInput.val().split(' ');
+			var timeStringSplit = timeString[1].split(':');
+			var hour = parseInt(timeStringSplit[0]);
+			var min = parseInt(timeStringSplit[1]);
+			
+			var selectedHour;
+			var selectedMins;
+
+			if(params.format.substr(6, 11) == 'h:i:a') {
+				var ampm = timeString[2];
+				selectedHour = mainContent.find(`.time-selector .select-hours .hour`).filter(function(){
+					return $(this).text() == hour;
+				});
+				selectedMins = mainContent.find(`.time-selector .select-minutes .minute`).filter(function(){
+					return $(this).text() == min;
+				});
+
+				selectedHour.parent().get(0).scrollTop = 0;
+				selectedMins.parent().get(0).scrollTop = 0;
+
+				selectedHour.addClass('selected');
+				selectedMins.addClass('selected');
+
+				selectedHour.parent().animate({
+					scrollTop: selectedHour.position().top - 100
+				}, 'fast');
+
+				selectedMins.parent().animate({
+					scrollTop: selectedMins.position().top - 100
+				}, 'fast');
+
+				mainContent.find(`.time-selector .am-pm`).removeClass('selected');
+
+				if(ampm == 'AM') 
+					mainContent.find(`.time-selector .am-mode-select`).addClass('selected');
+
+				if(ampm == 'PM') 
+					mainContent.find(`.time-selector .pm-mode-select`).addClass('selected');
+
+			}
+			else if(params.format.substr(6, 11) == 'h:i:s') {
+				mainContent.find(`.time-selector .am-pm`).removeClass('selected');
+
+				if(!parseInt(hour)) { /* 12 AM */
+					mainContent.find(`.time-selector .am-mode-select`).addClass('selected');
+					hour = 12;
+				}
+				else if(parseInt(hour)>12) { /* PM */
+					mainContent.find(`.time-selector .pm-mode-select`).addClass('selected');
+					hour = parseInt(hour) - 12;
+				}
+				else if(parseInt(hour)<12) { /* AM */
+					mainContent.find(`.time-selector .am-mode-select`).addClass('selected');
+				}
+				else {
+					mainContent.find(`.time-selector .pm-mode-select`).addClass('selected');
+				}
+
+				selectedHour = mainContent.find(`.time-selector .select-hours .hour`).filter(function(){
+					return $(this).text() == hour;
+				});
+
+				selectedMins = mainContent.find(`.time-selector .select-minutes .minute`).filter(function(){
+					return $(this).text() == min;
+				});
+
+				selectedHour.parent().get(0).scrollTop = 0;
+				selectedMins.parent().get(0).scrollTop = 0;
+
+				selectedHour.addClass('selected');
+				selectedMins.addClass('selected');
+
+				selectedHour.parent().animate({
+					scrollTop: selectedHour.position().top - 100
+				}, 'fast');
+
+				selectedMins.parent().animate({
+					scrollTop: selectedMins.position().top - 100
+				}, 'fast');
+			}
+		}
+
+
 		/*
 			function declarations end here
 		*/
@@ -274,6 +374,33 @@
 		});
 
 
+		/* adding time selectors if time mode is enabled */
+		if(params.time) {
+			mainContent.find(`.time-selector`).append(`<div class="select-hours-container">
+				<span class="up-arrow-small-hours"></span><div class="select-hours"></div>
+				<span class="down-arrow-small-hours"></span><div class="hour-footer">HOURS</div></div>
+				<div class="select-minutes-container"><span class="up-arrow-small-minutes">
+				</span><div class="select-minutes"></div><span class="down-arrow-small-minutes"></span>
+				<div class="minutes-footer">MIN</div></div><div class="am-pm-container">
+				<div class="am-pm am-mode-select">AM</div><div class="am-pm pm-mode-select">PM</div>
+				</div><span class="back-to-calendar">BACK</span>`);
+
+			for(var i=1; i<13; i++) {
+				mainContent.find(`.time-selector .select-hours`).append(
+					`<div class="hour" hour="` + i + `"> ` + i + `</div>`
+				);
+			}
+			for(var i=1; i<60; i++) {
+				mainContent.find(`.time-selector .select-minutes`).append(
+					`<div class="minute" minute="` + i + `"> ` + i + `</div>`
+				);
+			}
+		}
+		else {
+			mainContent.find(`.footer-buttons .set-time-button`).css('visibility', 'hidden');
+		}
+
+
 		/*
 			creating references
 		*/
@@ -289,20 +416,22 @@
 		selectYear = mainContent.find(`.year`);
 		selectYearContainer = selectYear.parent();
 
-		okButton = mainContent.find(`.ok-button span`);
+		setTimeButton = mainContent.find(`.footer-buttons .set-time-button`);
+		timeSelectorContainer = mainContent.find(`.time-selector`);
+
+		hourSelector = mainContent.find(`.time-selector .select-hours-container .hour`);
+		minuteSelector = mainContent.find(`.time-selector .select-minutes-container .minute`);
+
+		okButton = mainContent.find(`.footer-buttons .ok-button`);
 
 
-		if(!params.time) { /* if time mode is disabled */
 
-			if(inputboxDate.includes('-')) {
-				date_handler('-');
-			}
-			else if(inputboxDate.includes('/')) {
-				date_handler('/');
-			}
-
-		} /* if block ends here */
-
+		if(inputboxDate.includes('-')) {
+			date_handler('-');
+		}
+		else if(inputboxDate.includes('/')) {
+			date_handler('/');
+		}
 
 
 		/*
@@ -310,7 +439,7 @@
 		*/
 
 
-		/* month handlers  */
+		/* month handlers */
 
 		monthSelectorButton.click(() => {
 			mainContent.find(`.m-c-items`).hide();
@@ -350,7 +479,7 @@
 		});
 
 
-		/* year handlers  */
+		/* year handlers */
 
 		yearSelectorButton.click(() => {
 			mainContent.find(`.m-c-items`).hide();
@@ -389,6 +518,8 @@
 		});
 
 
+		/* input box handlers */
+
 		dateInput.focus((e) => {
 			var datepicker = $(e.currentTarget).next();
 			datepicker.css('left', dateInput.get(0).offsetLeft);
@@ -413,6 +544,9 @@
 			mainContent.find('.m-c-items').hide();
 			mainContent.find('.date-selector').show();
 
+			if(params.time)
+				read_time();
+
 			/* display datepicker */
 			datepicker.fadeIn();
 		});
@@ -421,7 +555,7 @@
 		dateInput.keydown(function(e) {
 			var code = e.keyCode || e.which;
 			if (code == '9') { /* tab pressed */
-				$('.broomberg-d-p').fadeOut();
+				$('.mr-elite-d-p').fadeOut();
 			}
 		});
 
@@ -429,10 +563,12 @@
 		datepicker.bind('keydown', function(e) {
 			var code = e.keyCode || e.which;
 			if (code == '9') { /* tab pressed */
-				$('.broomberg-d-p').fadeOut();
+				$('.mr-elite-d-p').fadeOut();
 			}
 		});
 
+
+		/* prev & next button handlers */
 
 		prevButton.click(() => {
 			if(currentMonthSelected != 1) {
@@ -484,9 +620,11 @@
 		});
 
 
+		/* date selection handler */
+
 		datepicker.on('click', '.main-content .date-selector .date', (e) => {
 			e = $(e.currentTarget);
-			
+
 			$('.main-content .date-selector .date').removeClass('selected');
 			e.addClass('selected');
 
@@ -496,14 +634,28 @@
 					.toString().substr(0,3) );
 
 			datepickerHeader.find(`.date`).text(currentDateSelected);
-		})
+		});
 
+
+		/* set time button handler */
+
+		setTimeButton.click(() => {
+			$('.main-content .m-c-items').hide();
+			mainContent.find(`.time-selector`).css('left', '200px').show();
+			mainContent.find(`.time-selector`).animate({left:0}, 200);
+			read_time();
+		});
+
+
+		/* final ok button handlers */
 
 		okButton.click(() => {
 			dateInput.val(get_new_date());
-			$('.broomberg-d-p').fadeOut();
+			$('.mr-elite-d-p').fadeOut();
 		});
 
+
+		/* document click handler */
 
 		$(document).click(function (e) {
 			if((!datepicker.is(e.target) && datepicker.has(e.target).length === 0) && !dateInput.is(e.target))
@@ -513,14 +665,104 @@
 		});
 
 
+		/* hours & minutes scroll handlers */
+
+		mainContent.find('.time-selector .up-arrow-small-hours').click(() => {
+			var posToScroll = hourSelector.parent().scrollTop();
+			hourSelector.parent().animate({
+				scrollTop:  posToScroll - 80 /* scroll up */
+			});
+		});
+
+		mainContent.find('.time-selector .down-arrow-small-hours').click(() => {
+			var posToScroll = hourSelector.parent().scrollTop();
+			hourSelector.parent().animate({
+				scrollTop:  posToScroll + 80 /* scroll down */
+			});
+		});
+
+		mainContent.find('.time-selector .up-arrow-small-minutes').click(() => {
+			var posToScroll = minuteSelector.parent().scrollTop();
+			minuteSelector.parent().animate({
+				scrollTop:  posToScroll - 80 /* scroll up */
+			});
+		});
+
+		mainContent.find('.time-selector .down-arrow-small-minutes').click(() => {
+			var posToScroll = minuteSelector.parent().scrollTop();
+			minuteSelector.parent().animate({
+				scrollTop:  posToScroll + 80 /* scroll down */
+			});
+		});
+
+
+		/* time selection handlers */
+
+		mainContent.find('.time-selector .hour').click((e) => {
+			e = $(e.currentTarget);
+			mainContent.find('.time-selector .hour').removeClass('selected');
+			e.addClass('selected');
+		});
+
+		mainContent.find('.time-selector .minute').click((e) => {
+			e = $(e.currentTarget);
+			mainContent.find('.time-selector .minute').removeClass('selected');
+			e.addClass('selected');
+		});
+
+		mainContent.find('.time-selector .am-pm').click((e) => {
+			e = $(e.currentTarget);
+			mainContent.find('.time-selector .am-pm').removeClass('selected');
+			e.addClass('selected');
+		});
+
+
+		/* save selected time handler */
+
+		mainContent.find('.time-selector .back-to-calendar').click(() => {
+			var hours;
+			var mins;
+			var seconds;
+			hours = mainContent.find('.time-selector .select-hours .selected').attr('hour');
+			mins = mainContent.find('.time-selector .select-minutes .selected').attr('minute');
+
+			if(mainContent.find('.time-selector .am-pm.selected').hasClass('am-mode-select')) {
+				if(params.format.substr(6, 11) === 'h:i:s') {
+
+					setTime = hours == '12' ?
+						'00:' + pad_date(mins, 2) + ':00' 
+						: pad_date(hours, 2) + ':' + pad_date(mins, 2) + ':00';
+				}
+				else if(params.format.substr(6, 11) === 'h:i:a') {
+					setTime = pad_date(hours, 2) + ':' + pad_date(mins, 2) + ' AM';
+				}
+			}
+			else if(mainContent.find('.time-selector .am-pm.selected').hasClass('pm-mode-select')) {
+				if(params.format.substr(6, 11) === 'h:i:s') {
+					hours = hours == '12' ? '12' : parseInt(hours) + 12;
+					setTime = pad_date(hours, 2) + ':' + pad_date(mins, 2) + ':00';
+				}
+				else if(params.format.substr(6, 11) === 'h:i:a') {
+					setTime = pad_date(hours, 2) + ':' + pad_date(mins, 2) + ' PM';
+				}
+			}
+
+			mainContent.find('.m-c-items').hide();
+			mainContent.find(`.date-selector`).css('left', '-200px').show();
+			mainContent.find(`.date-selector`).animate({left:0}, 200);
+		});
+
+
+		/* window change handler */
+
 		if (/*@cc_on!@*/false) { /* check for Internet Explorer */
 			document.onfocusout = function(){
-				$('.broomberg-d-p').fadeOut();
+				$('.mr-elite-d-p').fadeOut();
 			}
 		}
 		else {
 			window.onblur = function(){
-				$('.broomberg-d-p').fadeOut();
+				$('.mr-elite-d-p').fadeOut();
 			}
 		}
 
@@ -529,7 +771,7 @@
 
 	var destroy_datepicker = (dateInput) => {
 		dateInput.off();
-		if(dateInput.next().hasClass('broomberg-d-p')) {
+		if(dateInput.next().hasClass('mr-elite-d-p')) {
 			dateInput.next().remove();
 		}
 	};
